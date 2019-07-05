eb337e19f43b713246651a5f5abb7e27f86d862e
Adriano-Defilippis <ciprianoadrianodefilippis@gmail.com>
2019-07-05 14:12:14 +0200

    Ciclo for per chiedere un input all'utente 6 volte, ed ogni volta lo aggiunge a myArray

---
+++ JSnack #5/script.js
@@ -0,0 +1,16 @@
+// Creo un Array vuote;
+// Ciedo all'utente per sei volte un input
+// Se l'input è dispari lo inserisco nell'Array
+
+var myArray, input;
+
+myArray = [];
+
+//Ciclo for per chiedere un input all'utente 6 volte ed ogni volta lo aggiunge a myArray
+for (var i = 0; i < 6; i++) {
+  input = parseInt(prompt('Inserisci un valore'));
+  myArray.push(input);
+}
+
+//Restituzione dell'Array dopo il ciclo di Input
+console.log(myArray);
